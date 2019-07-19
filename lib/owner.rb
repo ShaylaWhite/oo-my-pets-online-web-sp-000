17ef48fcebca57296324fb19406c0cc5071f653a
Shayla White <shayladwhite1@gmail.com>
2019-07-19 01:48:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,8 +54,8 @@ def feed_cats
 end
 end
 def sell_pets
-  @pets = @dogs + @cats
-  @pets.collect do |species, instances|
+  @species = @dogs + @cats
+  @species.collect do |species, instances|
       
       instances.each do |pet|
         binding.pry
